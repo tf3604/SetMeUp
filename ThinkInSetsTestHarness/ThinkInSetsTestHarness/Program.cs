7e9d86ea2e5fd38bf4632c9adc2527797921b796
Brian Hansen <brian@tf3604.com>
2017-06-20 16:29:37 -0500

    Initial add of test harness.

---
+++ ThinkInSetsTestHarness/ThinkInSetsTestHarness/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThinkInSetsTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TimeSpan> executionTimes = new List<TimeSpan>();
+            for (int executionCounter = 0; executionCounter < 5; executionCounter++)
+            {
+                Stopwatch clock = Stopwatch.StartNew();
+                // Execute test
+                clock.Stop();
+                executionTimes.Add(clock.Elapsed);
+            }
+            executionTimes.Sort((x, y) => x.CompareTo(y));
+            executionTimes.RemoveAt(executionTimes.Count - 1);
+            executionTimes.RemoveAt(0);
+            double averageTimeInMilliseconds = executionTimes.Average(t => t.TotalMilliseconds);
+        }
+    }
+}
