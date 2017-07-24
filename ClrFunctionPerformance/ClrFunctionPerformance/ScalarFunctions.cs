4e8466f8a2deb4fb9c924aab703fc65caaf997d9
Brian Hansen <brian@tf3604.com>
2017-07-24 17:57:12 -0500

    Add CLR function solution.

---
+++ ClrFunctionPerformance/ClrFunctionPerformance/ScalarFunctions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static int ConstantFunc()
+    {
+        return 0;
+    }
+
+    [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.Read)]
+    public static int DataAccessFunc(int orderId)
+    {
+        //SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+        //sb.DataSource = @".\sql2016";
+        //sb.InitialCatalog = "CorpDB";
+        //sb.IntegratedSecurity = true;
+
+        using (SqlConnection connection = new SqlConnection("context connection=true"))
+        {
+            connection.Open();
+            string sql = @"select top 1 ProductId from dbo.OrderDetail od where od.OrderId = @OrderId order by OrderDetailId;";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.Parameters.Add(new SqlParameter("@OrderId", orderId));
+                int count = (int)command.ExecuteScalar();
+                return count;
+            }
+        }
+    }
+}
