4d22ab97cf6fd109cffa6b8ebf1c8cc9a55cf034
Brian Hansen <brian@tf3604.com>
2017-07-27 10:58:54 -0500

    Add MIT license to source files.

--- ThinkingInSets/ThinkingInSets/014 - Set-based Order.sql
+++ ThinkingInSets/ThinkingInSets/014 - Set-based Order.sql
@@ -1,3 +1,10 @@
+-----------------------------------------------------------------------------------------------------------------------
+-- 014 - Set-based Order.sql
+-- Version 1.0.0
+-- Look for the most recent version of this script at www.tf3604.com/sets.
+-- MIT License.  See the bottom of this file for details.
+-----------------------------------------------------------------------------------------------------------------------
+
 use CorpDB;
 go
 
@@ -45,3 +52,25 @@ with MostRecentTestRun as
 select ID, TestName, StartTime, EndTime, RunTimeMs,
 	(select avg(RunTimeMs) from MiddleRuns) AvgRunTimeMs
 from MiddleRuns;
+
+-----------------------------------------------------------------------------------------------------------------------
+-- Copyright 2017, Brian Hansen (brian at tf3604.com).
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+-----------------------------------------------------------------------------------------------------------------------
